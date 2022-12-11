b8f2ee5b5d078353b08267d67b486dd133cada8e
drzukus <alden.ys.lo@gmail.com>
2022-12-11 14:12:47 -0800

    popup

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${isRead ? "read" : "not read yet"}`;
-  }
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    const title = document.getElementById('bTitle').value;
+    const author = document.getElementById('bAuthor').value;
+    const pages = document.getElementById('bPages').value;
+    const isRead = document.getElementById('bRead').checked;
+    myLibrary.push(new Book(title, author, pages, isRead))
 }
+
+const bForm = document.getElementById("add-book-form");
+const addBookPopup = document.getElementById("add-Popup")
+const addBookBtn = document.getElementById("add-btn");
+
+bForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    console.log(myLibrary)
+
+    addBookPopup.style.display = "none"
+});
+
+
+addBookBtn.addEventListener("click", () => {
+    addBookPopup.style.display = "block";
+})
