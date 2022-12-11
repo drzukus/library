7da50bfd414cf60dcddbb2485edcf3c24d5ef1e8
drzukus <alden.ys.lo@gmail.com>
2022-12-11 11:29:42 -0800

    stuff

--- script.js
+++ script.js
@@ -6,13 +6,9 @@ function Book(title, author, pages, isRead) {
   this.pages = pages;
   this.isRead = isRead;
   this.info = function() {
-    console.log(`${title} by ${author}, ${pages} pages, ${isRead ? "read" : "not read yet"}`);
+    return `${title} by ${author}, ${pages} pages, ${isRead ? "read" : "not read yet"}`;
   }
 }
-
-
-const book = new Book("The Hobbit", "J.R.R. Tolkein", 295, false)
-book.info()
 
 function addBookToLibrary() {
   // do stuff here
